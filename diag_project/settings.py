1407000452bd45f8fb07557b583ecbb26a85fbd3
Josiah DuBose <josiah7686@gmail.com>
2016-12-28 12:11:43 -0500

    initial commit

--- diag_project/settings.py
+++ diag_project/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1',  'pacific-depths-50874.herokuapp.com']
 
 INSTALLED_APPS = [
     'diag_app.apps.DiagAppConfig',
