d0f983e111279c7b2a5c37de261a0bf377ed6468
Josiah DuBose <josiah7686@gmail.com>
2017-01-09 13:23:25 -0500

    created csv files and migrations for brand,model, and system

---
+++ migrate_data.py
@@ -0,0 +1,36 @@
+import csv
+
+def load_brands(apps, schema_editor):
+    with open("brand.csv", "r") as f:
+        dict_reader = csv.DictReader(f, fieldnames= ['name'])
+        Brand = apps.get_model("diag_app", "Brand")
+        for row in dict_reader:
+            brand_object = Brand(**row)
+            brand_object.save()
+
+
+def load_systems(apps, schema_editor):
+    with open("system.csv", "r") as f:
+        dict_reader = csv.DictReader(f, fieldnames= ['name'])
+        System = apps.get_model("diag_app", "System")
+        for row in dict_reader:
+            system_object = System(**row)
+            system_object.save()
+
+
+def load_models(apps, schema_editor):
+    with open("model.csv", "r") as f:
+        dict_reader = csv.DictReader(f, fieldnames= ['brand_id','name','year'])
+        Model = apps.get_model("diag_app", "Model")
+        for row in dict_reader:
+            model_object = Model(**row)
+            model_object.save()
+
+
+
+
+operations = [
+    migrations.RunPython(load_brands),
+    migrations.RunPython(load_systems),
+    migrations.RunPython(load_models),
+]
