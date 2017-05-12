0132c3edb29a704d35aa871a4a86d582d674a54c
Josiah DuBose <josiah7686@gmail.com>
2017-05-12 09:31:21 -0400

    update readme and setting.py

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 #Bike MD
 Live site: bike-md.herokuapp.com
 Bike MD is a community of motorcycle technicians, a place where they can connect with each other and ask technical questions, browse common problems and solutions by model, and help each other troubleshoot vehicles more efficiently. This was originally built as my final project for the Iron Yard. It was written in Python, Django, and JavaScript. Since then I have been making a list of improvements and
-bugs that need to be addressed. If you'd like to contribute, I'll will be opening a numbers of issues
-in the near future. Have a look at the to do list and reach out if something catches your eye.
-if something catches your eye.   
-TODO List:
-1. finish profile page.
-2. finish notifications.
-3. refactor JavaScript to remove repetitive functions. 
+bugs that need to be addressed.
+    TODO List:
+    1. finish profile page.
+    2. finish notifications.
+    3. refactor JavaScript to remove repetitive functions, on clicks etc...
+    4. refactor and clean up CSS.
+    *many more to come
---
+++ diag_project/settings_example.py
@@ -0,0 +1,124 @@
+import dj_database_url
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DEBUG = True
+
+### SECRET KEY ###
+#secret can be set as environment varible in your shell or directly set here.
+#SECRET_KEY = os.environ['SECRET_KEY']
+#SECRET_key = 'whjwxt79_m_8impz71nc1@qdzzh99z(h%tkuvrkb8r8f4e'
+#secret keys can be generated at http://www.miniwebtool.com/django-secret-key-generator/
+############
+
+#need for local development
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
+
+INSTALLED_APPS = [
+    'diag_app.apps.DiagAppConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'crispy_forms',
+
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'diag_project.urls'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 500,
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+}
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'diag_project.wsgi.application'
+
+#create postgreSQL DB with the name BIKEMD in order
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'BIKEMD',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+## Needed for deployment
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+# DATABASES['default'] = dj_database_url.config()
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LOGIN_REDIRECT_URL = '/diag_app/'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, '../diag_app/static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
